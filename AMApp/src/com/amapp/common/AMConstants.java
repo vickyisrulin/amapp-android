d0e49998d583f11d4ba57af6b30a970e035117ee
Darshan Desai <darshanbdesai@gmail.com>
2015-12-07 06:16:20 -0800

    added AMConstants - keep App specific constants here

---
+++ AMApp/src/com/amapp/common/AMConstants.java
@@ -0,0 +1,16 @@
+package com.amapp.common;
+
+/**
+ * Created by dadesai on 12/7/15.
+ */
+public class AMConstants {
+
+    public static final String AM_Application_Title = "Anoopam Mission";
+
+    // AMS Service Contstants
+    public static final String AMS_Domain_Url = "http://www.mocky.io";
+    public static final String AMS_ThakorjiToday_Endpoint = "/v2/565320442400005027629a41";
+    public static final String AMS_QuoteOfTheWeek_Endpoint = "/v2/5665890a250000602c996e79";
+    public static final String AMS_Request_Get_Temples_Tag = "ams_get_temples";
+    public static final String AMS_Request_Get_Quotes_Tag = "ams_get_quotes";
+}
