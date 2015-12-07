d29ff25b599b838767757ff4471156f39b90ce07
Darshan Desai <darshanbdesai@gmail.com>
2015-12-07 15:08:19 -0800

    adding ENV enum to easily switch between MOCK and LIVE

---
+++ AMApp/src/com/amapp/Environment.java
@@ -0,0 +1,53 @@
+package com.amapp;
+
+import com.amapp.common.AMConstants;
+
+/**
+ * Created by dadesai on 12/7/15.
+ */
+public enum Environment {
+
+    ENV_MOCK(AMConstants.MOCK_Domain_Url,
+            AMConstants.MOCK_ThakorjiToday_Endpoint_Suffix,
+            AMConstants.MOCK_QuoteOfTheWeek_Endpoint_Suffix
+            ),
+
+    ENV_LIVE(AMConstants.LIVE_Domain_Url,
+            AMConstants.LIVE_ThakorjiToday_Endpoint_Suffix,
+            AMConstants.LIVE_QuoteOfTheWeek_Endpoint_Suffix
+    );
+
+    private String mDomainUrl;
+    private String mThakorjiTodayEndpoint;
+    private String mQuoteOfTheDayEndpoint;
+
+    Environment(String domainUrl, String thakorjiTodayEndpointSuffix, String quoteOfTheDayEndpointSuffix) {
+        mDomainUrl = domainUrl;
+        mThakorjiTodayEndpoint = domainUrl+thakorjiTodayEndpointSuffix;
+        mQuoteOfTheDayEndpoint = domainUrl+quoteOfTheDayEndpointSuffix;
+    }
+
+    /**
+     * Returns the BASE URL of the Server Domain
+     * @return String
+     */
+    public String getDomainUrl() {
+        return mDomainUrl;
+    }
+
+    /**
+     * returns the Thakorji Today Endpoint
+     * @return String
+     */
+    public String getThakorjiTodayEndpoint() {
+        return mThakorjiTodayEndpoint;
+    }
+
+    /**
+     * returns the Quote of the Day Endpoint
+     * @return String
+     */
+    public String getQuoteOfTheDayEndpoint() {
+        return mQuoteOfTheDayEndpoint;
+    }
+}
