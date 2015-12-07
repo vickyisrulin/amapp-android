20e0893df60ae3a048382febd2d944613c59242c
Darshan Desai <darshanbdesai@gmail.com>
2015-12-07 09:04:07 -0800

    point to the AMS server instead of mock for thakorji today

--- AMApp/src/com/amapp/common/AMConstants.java
+++ AMApp/src/com/amapp/common/AMConstants.java
@@ -8,8 +8,8 @@ public class AMConstants {
     public static final String AM_Application_Title = "Anoopam Mission";
 
     // AMS Service Contstants
-    public static final String AMS_Domain_Url = "http://www.mocky.io";
-    public static final String AMS_ThakorjiToday_Endpoint = "/v2/565320442400005027629a41";
+    public static final String AMS_Domain_Url = "http://anoopam.org";
+    public static final String AMS_ThakorjiToday_Endpoint = "/api/thakorji.php";
     public static final String AMS_QuoteOfTheWeek_Endpoint = "/v2/5665890a250000602c996e79";
     public static final String AMS_Request_Get_Temples_Tag = "ams_get_temples";
     public static final String AMS_Request_Get_Quotes_Tag = "ams_get_quotes";
