9b30dc454bada926e6e2bbb0ff803b72960c42a1
Darshan Desai <darshanbdesai@gmail.com>
2015-12-10 19:31:43 -0800

    updated AMConstants to use lastUpdatedTimestamp param in thakorjiToday

--- AMApp/src/com/amapp/common/AMConstants.java
+++ AMApp/src/com/amapp/common/AMConstants.java
@@ -7,17 +7,17 @@ public class AMConstants {
 
     //MOCK MOCKY
     public static final String MOCK_MOCKY_Domain_Url = "http://www.mocky.io";
-    public static final String MOCK_MOCKY_ThakorjiToday_Endpoint_Suffix = "/v2/565320442400005027629a41";
+    public static final String MOCK_MOCKY_ThakorjiToday_Endpoint_Suffix = "/v2/565320442400005027629a41?lastUpdatedTimestamp=%s";
     public static final String MOCK_MOCKY_QuoteOfTheWeek_Endpoint_Suffix = "/v2/5665890a250000602c996e79";
 
     //MOCK APIARY
     public static final String MOCK_APIARY_Domain_Url = "http://www.mocky.io";
-    public static final String MOCK_APIARY_ThakorjiToday_Endpoint_Suffix = "/v2/565320442400005027629a41";
+    public static final String MOCK_APIARY_ThakorjiToday_Endpoint_Suffix = "/v2/565320442400005027629a41?lastUpdatedTimestamp=%s";
     public static final String MOCK_APIARY_QuoteOfTheWeek_Endpoint_Suffix = "/v2/5665890a250000602c996e79";
 
     //LIVE
     public static final String LIVE_Domain_Url = "http://anoopam.org";
-    public static final String LIVE_ThakorjiToday_Endpoint_Suffix = "/api/ams/v1/thakorji.php";
+    public static final String LIVE_ThakorjiToday_Endpoint_Suffix = "/api/ams/v1/thakorji.php?lastUpdatedTimestamp=%s";
     public static final String LIVE_QuoteOfTheWeek_Endpoint_Suffix = "/v2/5665890a250000602c996e79";
 
     // other AMS constants
