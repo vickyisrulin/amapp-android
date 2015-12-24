ee90a2ff2e10229797dc510aa6076cd37729840e
Darshan Desai <darshanbdesai@gmail.com>
2015-12-24 07:54:05 -0800

    added AMApplication - extending from SmartApplication - will be used throughout the App to refer to the Application

---
+++ AMApp/src/com/amapp/AMApplication.java
@@ -0,0 +1,32 @@
+package com.amapp;
+
+import com.amapp.common.AMServiceRequest;
+import com.smart.framework.SmartApplication;
+
+/**
+ * Created by dadesai on 12/23/15.
+ */
+public class AMApplication extends SmartApplication {
+
+    // change this to MOCK or LIVE to change all service calls
+    private Environment mEnvironment;
+
+    /**
+     * returns the Environment
+     * @return Environment
+     */
+    public Environment getEnv() {
+        return mEnvironment;
+    }
+
+    public static AMApplication getInstance() {
+        return (AMApplication) REF_SMART_APPLICATION;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mEnvironment = Environment.ENV_LIVE;
+        AMServiceRequest.getInstance().startThakorjiTodayUpdatesFromServer();
+    }
+}
