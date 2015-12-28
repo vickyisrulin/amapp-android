bbfd2a9fcb4359fe598563400b7090b588d602e0
Darshan Desai <darshanbdesai@gmail.com>
2015-12-28 07:52:24 -0800

    updated splash Activity to start using fetched contents from the server to splash the image

--- AMApp/src/com/amapp/SplashActivity.java
+++ AMApp/src/com/amapp/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.amapp;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
@@ -9,9 +11,15 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.transition.Fade;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
+import com.amapp.common.AMServiceRequest;
 import com.amapp.home.HomeListActivity;
-import com.amapp.thakorjitoday.TempleListActivity;
+import com.androidquery.AQuery;
+import com.smart.caching.SmartCaching;
+
+import java.util.ArrayList;
 
 /**
  * Created by tasol on 23/6/15.
@@ -20,6 +28,9 @@ import com.amapp.thakorjitoday.TempleListActivity;
 public class SplashActivity extends AMAppMasterActivity {
 
     Handler handler=new Handler();
+    ImageView splashImage;
+    FrameLayout mLayout;
+    private SmartCaching mSmartCaching;
 
     @Override
     public View getLayoutView() {
@@ -40,12 +51,33 @@ public class SplashActivity extends AMAppMasterActivity {
 
     @Override
     public void preOnCreate() {
+    }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mLayout = (FrameLayout) findViewById(getLayoutID());
+        splashImage = (ImageView) findViewById(R.id.splashscreen);
+        setSplashScreenImage();
+    }
+
+    private void setSplashScreenImage() {
+        AQuery aq =  AMApplication.getInstance().getAQuery();
+        String imageUrl = getSplashScreenUpdatedUrl();
+        aq.id(splashImage).image(imageUrl, true, true, getDeviceWidth(), R.drawable.splash);
+    }
+
+    private String getSplashScreenUpdatedUrl() {
+        ArrayList<ContentValues> splashScreen = mSmartCaching.getDataFromCache("splashMessages");
+        if (splashScreen != null && splashScreen.size() > 0) {
+            return splashScreen.get(0).getAsString("splashImage");
+        }
+        return "";
     }
 
     @Override
     public void initComponents() {
-
+        mSmartCaching = new SmartCaching(this);
     }
 
     @Override
@@ -65,16 +97,12 @@ public class SplashActivity extends AMAppMasterActivity {
 
     @Override
     public void setActionListeners() {
-
-
     }
 
     @Override
     public void manageAppBar(ActionBar actionBar, Toolbar toolbar,ActionBarDrawerToggle actionBarDrawerToggle) {
-
         actionBar.hide();
     }
-
 
     @Override
     public int getDrawerLayoutID() {
