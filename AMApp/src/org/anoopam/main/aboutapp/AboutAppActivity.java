781d9e0040b162907de5d56d266dd09f94ad0414
Darshan Desai <darshanbdesai@gmail.com>
2016-01-20 04:43:13 -0800

    added version# info in the AboutApp

--- AMApp/src/org/anoopam/main/aboutapp/AboutAppActivity.java
+++ AMApp/src/org/anoopam/main/aboutapp/AboutAppActivity.java
@@ -1,11 +1,14 @@
 package org.anoopam.main.aboutapp;
 
 import android.content.ContentValues;
+import android.content.pm.PackageInfo;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import org.anoopam.ext.smart.customviews.SmartTextView;
 import org.anoopam.main.AMAppMasterActivity;
 import org.anoopam.main.AMApplication;
+import org.anoopam.main.BuildConfig;
 import org.anoopam.main.R;
 import org.anoopam.main.common.TouchImageView;
 import com.androidquery.AQuery;
@@ -18,14 +21,14 @@ import java.util.ArrayList;
  */
 public class AboutAppActivity extends FragmentActivity {
 
-    private TouchImageView mAboutAppImage;
-    private SmartCaching mSmartCaching;
-
+    SmartTextView versionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_aboutapp);
-
+        versionTextView = (SmartTextView) findViewById(R.id.txtVersion);
+        String versionName = "Version: " + BuildConfig.VERSION_NAME;
+        versionTextView.setText(versionName);
     }
 }
