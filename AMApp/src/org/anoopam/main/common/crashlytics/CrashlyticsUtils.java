a6a49ee4505439d9a0c6e6723914854ceff71a1d
Darshan Desai <darshanbdesai@gmail.com>
2016-02-01 18:44:12 -0800

    added helper method to log into the Crashlytics. When Crash happens, Crashlytics will show all these logs for that crash

--- AMApp/src/org/anoopam/main/common/crashlytics/CrashlyticsUtils.java
+++ AMApp/src/org/anoopam/main/common/crashlytics/CrashlyticsUtils.java
@@ -1,5 +1,7 @@
 package org.anoopam.main.common.crashlytics;
 
+import com.crashlytics.android.Crashlytics;
+
 /**
  * Created by dadesai on 2/1/16.
  */
@@ -10,5 +12,13 @@ public class CrashlyticsUtils {
      */
     public static void forceCrash() {
         throw new RuntimeException("This is a Forced Crash for testing");
+    }
+
+    /***
+     * Logs the message in Crashlytics
+     * @param message
+     */
+    public static void crashlyticsLog(String message) {
+        Crashlytics.log(message);
     }
 }
