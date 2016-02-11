75f867be3fdd9b7e76d9412c8adefa0a9f2b5baa
Shanu Nigam <shanu.nigam@gmail.com>
2016-02-10 20:57:30 -0800

    Added Copyright text to 49 .java files in the org/anoopam/main package

--- AMApp/src/org/anoopam/main/SplashActivity.java
+++ AMApp/src/org/anoopam/main/SplashActivity.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2016. Anoopam Mission. This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License at "http://www.gnu.org/licenses/licenses.html" for more details.
+ *
+ */
+
 package org.anoopam.main;
 
 import android.content.ContentValues;
