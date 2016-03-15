37fb0d016072fb6cb795fd79c0f10e60a11ede59
Darshan Desai <darshanbdesai@gmail.com>
2016-03-14 18:02:59 -0700

    enable share and download feature for SahebjiDarshan and Quote of the Week (Thanks Shanu!)

--- AMApp/src/org/anoopam/main/qow/QuoteActivity.java
+++ AMApp/src/org/anoopam/main/qow/QuoteActivity.java
@@ -9,36 +9,98 @@ package org.anoopam.main.qow;
 
 import android.content.ContentValues;
 
+import org.anoopam.ext.smart.customviews.Log;
 import org.anoopam.ext.smart.framework.SmartUtils;
 import org.anoopam.main.AMAppMasterActivity;
 import org.anoopam.main.R;
 import org.anoopam.main.common.DataDownloadUtil;
 import org.anoopam.main.common.TouchImageView;
 
+import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.transition.Slide;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.URLUtil;
 
 import org.anoopam.ext.smart.caching.SmartCaching;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class QuoteActivity extends AMAppMasterActivity {
 
     private TouchImageView mQuoteImage;
     private SmartCaching mSmartCaching;
+    private String downloadFileName;
+    private String downloadFilePath;
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_menu_home, menu);
+        return true;
+    }
 
     private void setQuoteImage() {
         String imageUrl = getQuoteUpdatedUrl();
+        downloadFileName= URLUtil.guessFileName(imageUrl, null, null);
+        downloadFilePath = SmartUtils.getAnoopamMissionImageStorage()+ File.separator;
+
         final File destination = new File(SmartUtils.getAnoopamMissionImageStorage()+ File.separator + URLUtil.guessFileName(imageUrl, null, null));
         Uri downloadUri = Uri.parse(imageUrl.replaceAll(" ", "%20"));
         DataDownloadUtil.downloadImageFromServerAndRender(downloadUri, destination, mQuoteImage);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_download:
+                saveImageToGallery();
+                return true;
+
+            case R.id.action_share:
+                shareImage();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    protected void saveImageToGallery() {
+        String fromDir = downloadFilePath;
+        String toDir = Environment.getExternalStorageDirectory().getAbsolutePath()+File.separator+"AnoopamMission"+File.separator;
+        File checkfile = new File(fromDir+downloadFileName);
+        if (checkfile.isFile()) {
+            SmartUtils.copyFile(fromDir, downloadFileName, toDir);
+        }
+        SmartUtils.ting(this, "Downloaded Successfully");
+        //  MediaScannerConnection mediaScanner =  new MediaScannerConnection(getApplicationContext(),null);
+        //  mediaScanner.scanFile(toDir+File.separator+downloadFileName, null);
+    }
+
+    protected void shareImage () {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("image/*");
+        String combinedImagePath = downloadFilePath+downloadFileName;
+        try {
+            String path = MediaStore.Images.Media.insertImage(getContentResolver(), combinedImagePath, downloadFileName, null);
+            Uri imageUri = Uri.parse(path);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
+            startActivity(Intent.createChooser(shareIntent, "Share via"));
+        } catch (Exception e)
+        {
+            Log.e("tag", e.getMessage());
+        }
+
     }
 
     private String getQuoteUpdatedUrl() {
--- AMApp/src/org/anoopam/main/sahebjidarshan/SahebjiDarshanActivity.java
+++ AMApp/src/org/anoopam/main/sahebjidarshan/SahebjiDarshanActivity.java
@@ -9,18 +9,24 @@ package org.anoopam.main.sahebjidarshan;
 
 import android.content.ContentValues;
 
+import org.anoopam.ext.smart.customviews.Log;
 import org.anoopam.ext.smart.framework.SmartUtils;
 import org.anoopam.main.AMAppMasterActivity;
 import org.anoopam.main.R;
 import org.anoopam.main.common.DataDownloadUtil;
 import org.anoopam.main.common.TouchImageView;
 
+import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.transition.Slide;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.URLUtil;
 
@@ -28,12 +34,22 @@ import org.anoopam.ext.smart.caching.SmartCaching;
 import org.anoopam.main.common.crashlytics.CrashlyticsUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class SahebjiDarshanActivity extends AMAppMasterActivity {
 
     private TouchImageView mSahebjiDarshanImage;
     private SmartCaching mSmartCaching;
+    private String downloadFileName;
+    private String downloadFilePath;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_menu_home, menu);
+        return true;
+    }
+
 
     @Override
     protected void onResume() {
@@ -43,6 +59,9 @@ public class SahebjiDarshanActivity extends AMAppMasterActivity {
 
     private void setSahebjiDarshanImage() {
         String imageUrl = getSahebjiDarshanUpdatedUrl();
+        downloadFileName= URLUtil.guessFileName(imageUrl, null, null);
+        downloadFilePath = SmartUtils.getAnoopamMissionDailyRefreshImageStorage() + File.separator;
+
         final File destination = new File(SmartUtils.getAnoopamMissionDailyRefreshImageStorage() + File.separator + URLUtil.guessFileName(imageUrl, null, null));
         Uri downloadUri = Uri.parse(imageUrl.replaceAll(" ", "%20"));
         DataDownloadUtil.downloadImageFromServerAndRender(downloadUri, destination, mSahebjiDarshanImage);
@@ -119,4 +138,50 @@ public class SahebjiDarshanActivity extends AMAppMasterActivity {
             return true;
         }
     }
+
+    public boolean onOptionsItemSelected(MenuItem item)  {
+        switch (item.getItemId()) {
+            case R.id.action_download:
+                saveImageToGallery();
+                return true;
+
+            case R.id.action_share:
+                shareImage();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    protected void saveImageToGallery() {
+        String fromDir = downloadFilePath;
+        String toDir = Environment.getExternalStorageDirectory().getAbsolutePath()+File.separator+"AnoopamMission"+File.separator;
+        File checkfile = new File(fromDir+downloadFileName);
+        if (checkfile.isFile()) {
+            SmartUtils.copyFile(fromDir, downloadFileName, toDir);
+        }
+        SmartUtils.ting(this, "Downloaded Successfully");
+        //  MediaScannerConnection mediaScanner =  new MediaScannerConnection(getApplicationContext(),null);
+        //  mediaScanner.scanFile(toDir+File.separator+downloadFileName, null);
+    }
+
+    protected void shareImage () {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("image/*");
+        String combinedImagePath = downloadFilePath+downloadFileName;
+        try {
+            String path = MediaStore.Images.Media.insertImage(getContentResolver(), combinedImagePath, downloadFileName, null);
+            Uri imageUri = Uri.parse(path);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
+            startActivity(Intent.createChooser(shareIntent, "Share via"));
+        } catch (Exception e)
+        {
+            Log.e("tag", e.getMessage());
+        }
+
+    }
+
+
+
 }
