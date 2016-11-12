43e9bb4ec9fc6c4f70c9eef149d9c6ab7fd1f011
Darshan Desai <darshanbdesai@gmail.com>
2016-11-12 08:56:13 -0800

    adding new build script

---
+++ generate-build.sh
@@ -0,0 +1,16 @@
+## - Generate release build and SIGN manually using Android Studio
+./gradlew clean
+./gradlew assembleRelease
+ls AMApp/build/outputs/apk/
+$HOME/adt/sdk//build-tools/23.0.1/zipalign -v -p 4 AMApp/build/outputs/apk/AMApp-release-unsigned.apk AMApp/build/outputs/apk/AMApp-release-unsigned2.apk
+mv AMApp/build/outputs/apk/AMApp-release-unsigned2.apk AMApp/build/outputs/apk/AMApp-release-unsigned.apk
+ls AMApp/build/outputs/apk/
+
+## - SIGN the AMApp/build/outputs/apk/AMApp-release-unsigned.apk manually using Android Studio
+
+
+## -- for debug build
+## ./gradlew clean
+## ./gradlew assembleDebug
+
+
