e0311687f15106d32e961746b12643df2e0df131
VR <vickyisrulin@gmail.com>
2017-03-10 23:11:18 -0800

    new commit

--- generate-build.sh
+++ generate-build.sh
@@ -1,6 +1,6 @@
 ## - Generate release build and SIGN manually using Android Studio
 ./gradlew clean
-./gradlew assembleRelease
+./gradlew assembleRelease #sasdaf
 ls AMApp/build/outputs/apk/
 $HOME/adt/sdk//build-tools/23.0.1/zipalign -v -p 4 AMApp/build/outputs/apk/AMApp-release-unsigned.apk AMApp/build/outputs/apk/AMApp-release-unsigned2.apk
 mv AMApp/build/outputs/apk/AMApp-release-unsigned2.apk AMApp/build/outputs/apk/AMApp-release-unsigned.apk
